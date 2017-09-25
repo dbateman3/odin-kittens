2699a664448340af76fbf0835458b19c4b52f547
Dan Bateman <dbateman0319@starkstate.net>
2017-09-24 22:50:56 -0400

    Controller actions and views created

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,34 @@
 class KittensController < ApplicationController
+	def index
+		@kittens = Kitten.all
+	end
+
+	def new
+		@kitten = Kitten.new
+	end
+
+	def create
+		@kitten = Kitten.new(kitten_params)
+		@kitten.save
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		@kitten.update(kitten_params)
+	end
+
+	def destroy
+		@kitten.destroy
+	end
+
+	private
+
+	def kitten_params
+		params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+	end
 end
